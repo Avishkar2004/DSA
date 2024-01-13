c458c965854afdc674b2758f75311cebd369fc19
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-01-13 09:47:14 +0530

    Sorting

--- 3. Arrays/index.js
+++ 3. Arrays/index.js
@@ -1,8 +1,15 @@
-function summation(n) {
-  let sum = 0;
-  for(let i = 1; i <=n; i++) {
-      sum += i
+let data = [40, 30, 12, 25];
+for (let i = 0; i < data.length; i++) {
+  console.log(i);
+  for (let j = 0; j < data.length; j++) {
+    console.log(data); //loop iterate 4 times  0 1 2 3 * 4
+    if(data[j] > data[j + 1]){
+      let temp = data[j]
+      data[j] = data[ j + 1]
+      data[j + 1] = temp;
+    }
   }
-   return sum;
 }
 
+
+console.log(data);
