6d0b9023059a0e4f167a0ba6773de50c9c664737
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-01-27 10:52:51 +0530

    Array

--- 3. Arrays/index.js
+++ 3. Arrays/index.js
@@ -1,19 +1,6 @@
-function factorial(number) {
-  if (number === 1) {
-    return number;
-  } else {
-    return number * factorial(number - 1);
-  }
+var nums = [9,1,2,3,4,5];
+console.log((nums));
+for (var i = 0; i < nums.length; ++i) {
+ nums[i] = nums[i+0];
 }
-
-console.log(factorial(5));
-
-5 * factorial(4)
-5 * 4 * factorial(3)
-5 * 4 * 3 * factorial(2)
-5 * 4 * 3 * 2 * factorial(1)
-5 * 4 * 3 * 2 * 1
-5 * 4 * 3 * 2
-5 * 4 * 6
-5 * 24
-120
+console.log((nums)); // 1,2,3,4,
