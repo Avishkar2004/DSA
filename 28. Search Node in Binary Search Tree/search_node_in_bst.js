33446671de5429ec79dfa21e0cc06b490ba05877
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-03-12 10:13:25 +0530

    Search node in binary Search Tree

---
+++ 28. Search Node in Binary Search Tree/search_node_in_bst.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BSTree {
+  constructor() {
+    this.root = null;
+  }
+  isTreeEmpty() {
+    return this.root === null;
+  }
+  makeTree(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  //! this is for left node
+  insertNode(root, newNode) {
+    if (root.value > newNode.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+      //! this is for right node
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+  //! for searching
+  search(root, val) {
+    if (root === null) {
+      return false;
+    } else if (root.value === val) {
+      return true;
+    } else if (root.value > val) {
+      return this.search(root.left, val);
+    } else {
+      return this.search(root.right, val);
+    }
+  }
+}
+
+//! This is how we invoked Tree
+let bst = new BSTree();
+bst.makeTree(20);
+bst.makeTree(10);
+bst.makeTree(5);
+bst.makeTree(30);
+bst.makeTree(40);
+bst.makeTree(23);
+console.log(bst.search(bst.root, 30));
+
+console.log(bst);
+// console.log(bst.isTreeEmpty());
