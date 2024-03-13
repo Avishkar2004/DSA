9684a0a16e1be9ce7f717c4c3331cd61cf14f926
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-03-13 08:31:19 +0530

    Find Min and Max Value in Binary Search Tree

---
+++ 31. Find Min and Max Value in Binary Search Tree/index.js
@@ -0,0 +1,130 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BSTree {
+  constructor() {
+    this.root = null;
+  }
+  isTreeEmpty() {
+    return this.root === null;
+  }
+  makeTree(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  //! this is for left node
+  insertNode(root, newNode) {
+    if (root.value > newNode.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+      //! this is for right node
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+  //! for searching
+  search(root, val) {
+    if (root === null) {
+      return false;
+    } else if (root.value === val) {
+      return true;
+    } else if (root.value > val) {
+      return this.search(root.left, val);
+    } else {
+      return this.search(root.right, val);
+    }
+  }
+
+  //! BST In Binary Search (preOrder)
+  preOrder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preOrder(root.left);
+      this.preOrder(root.right);
+    }
+  }
+
+  //! BST In Binary Search (Inorder)
+  InOrder(root) {
+    if (root) {
+      this.InOrder(root.left);
+      //   console.log(root.value);
+      this.InOrder(root.right);
+    }
+  }
+  //! BST In Binary Search (postorder)
+  postOrder(root) {
+    if (root) {
+      this.postOrder(root.left);
+      this.postOrder(root.right);
+      console.log(root.value);
+    }
+  }
+  //! Breadth First Search Traversing in Binary Search Tree
+  bfsTraversing() {
+    let queue = [];
+    queue.push(this.root);
+    while (queue.length) {
+      let current = queue.shift();
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+      console.log(current.value);
+    }
+  }
+
+  //! this is for finding min values
+  minValue(root) {
+    if (!root.left) {
+      return root.value;
+    } else {
+      return this.minValue(root.left);
+    }
+  }
+
+  //! this is for finding max values
+  maxValue(root) {
+    if (!root.right) {
+      return root.value;
+    } else {
+      return this.maxValue(root.right);
+    }
+  }
+}
+
+//! This is how we invoked Tree
+let bst = new BSTree();
+bst.makeTree(20);
+bst.makeTree(10);
+bst.makeTree(5);
+bst.makeTree(11);
+bst.makeTree(30);
+bst.makeTree(40);
+bst.makeTree(23);
+
+// bst.preOrder(bst.root);
+// bst.InOrder(bst.root);
+// bst.postOrder(bst.root);
+// bst.bfsTraversing(); // 20,10,30,5,11,23,40
+
+console.log(bst.minValue(bst.root)); // 5
+console.log(bst.maxValue(bst.root)); // 40
