1dcf83438b432225f6ecf169afc3f0714a5ca713
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-06-05 20:49:52 +0530

    Array Sorting

--- 3. Arrays/index.js
+++ 3. Arrays/index.js
@@ -1,25 +1,15 @@
-let data1 = [3, 7, 12, 34, 56, 90];
-let data2 = [4, 9, 25, 45];
-let data3 = [];
-let d1 = 0;
-let d2 = 0;
-let d3 = 0;
+// let data = [40, 30, 12, 25];
+let data = [34, 3, 1, 25];
 
-while (d1 < data1.length && d2 < data2.length) {
-  if (data1[d1] < data2[d2]) {
-    data3[d3] = data1[d1];
-    d1++;
-  } else {
-    data3[d3] = data2[d2];
-    d2++;
+for (let i = 0; i < data.length; i++) {
+  for (j = 0; j < data.length; j++) {// 0,1,2,3 * 4
+    console.log(data);
+    if (data[j] > data[j + 1]) {
+      let temp = data[j];
+      data[j] = data[j + 1];
+      data[j + 1] = temp;
+    }
   }
-  d3++;
 }
 
-while (d1 < data1.length) {
-  data3[d3] = data1[d1];
-  d1++;
-  d3++;
-}
-
-console.log(data3);
+console.log(data);
