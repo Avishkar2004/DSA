81cb6c9c120c01e4f230049fa5fb2ed2781d6f03
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-06-08 21:08:46 +0530

    Stack with class

--- 6. Stack Data Structure/index.js
+++ 6. Stack Data Structure/index.js
@@ -1,36 +1,32 @@
-//array :-
+//! This is our stack
+let data = [];
 
-const data = [];
 let currentSize = data.length;
+
 let maxValue = 5;
 
 function push(newVal) {
-  if (currentSize >= maxValue) {
-    alert("stack is full, you can't add" + newVal);
-  }
-
   data[currentSize] = newVal;
-  currentSize += 1;
+  currentSize++;
 }
 
 function pop() {
-  if (currentSize > 0) {
-    currentSize -= 1;
-    data.length = currentSize;
-  } else {
-    alert("stack is already empty");
+  lastremovedItem = data[currentSize - 1];
+  currentSize--;
+  data.length = currentSize;
+  return lastremovedItem;
+}
+
+function reverseStr(item) {
+  for (let i = 0; i < item.length; i++) {
+    push(item[i]);
+  }
+  for (let i = 0; i < item.length; i++) {
+    item[i] = pop();
   }
 }
 
-push(20);
-push(1);
-push(21);
-push(20);
-push(2);
-pop();
-pop();
-pop();
-pop();
-pop();
-pop()
-console.log(data);
+let str = "Vicky";
+str = str.split("");
+reverseStr(str);
+console.log(str);
--- 6. Stack Data Structure/stack_with_class.js
+++ 6. Stack Data Structure/stack_with_class.js
@@ -1,15 +1,38 @@
-class Avishkar {
-  //! why constructor :- jab bhi hum log class ko call karte hai tab sabse pahle constructor call hota hai
- item = []
- currentSize
- maxSize;
-  constructor() {
-    console.log("first")
+class Stack {
+  item = [];
+  currentSize;
+  maxSize;
+  constructor(size) {
+    this.maxSize = size;
+    this.currentSize = this.item.length;
   }
-  push() {}
 
-  pop() {}
+  push(newVal) {
+    if (this.currentSize >= this.maxSize) {
+      console.log("Stack is full");
+    } else {
+      this.item[this.currentSize] = newVal;
+      this.currentSize++;
+    }
+  }
+  pop() {
+    if (this.currentSize > 0) {
+      this.currentSize--;
+      this.item.length = this.currentSize;
+    } else {
+      console.log("Stack is already empty");
+    }
+  }
+  display() {
+    console.log(this.item);
+  }
 }
 
-
-st1 = new Avishkar()
+st1 = new Stack(5);
+st1.push(40);
+st1.push(34);
+st1.push(78);
+st1.push(2);
+st1.push(28);
+st1.pop();
+st1.display();
