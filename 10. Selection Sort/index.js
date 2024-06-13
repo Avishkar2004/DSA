4bfc08e41f58ebd0800fb9f453f959e092337fae
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-06-13 19:25:02 +0530

    Selection Sort

--- 10. Selection Sort/index.js
+++ 10. Selection Sort/index.js
@@ -1,16 +1,14 @@
-//This is ascending order
-let items = [20, 12, 53, 3, 8, 760];
+let items = [76, 45, 124, 63, 83, 12];
+
 function SelectionSort(data) {
-  //! console.log(data); [ 20, 12, 53, 3 ]
   let minId;
   for (let i = 0; i < data.length; i++) {
     minId = i;
     for (let j = i; j < data.length; j++) {
-      if (data[j] < data[minId]) {
+      if (data[j] > data[minId]) {
         minId = j;
       }
     }
-    //! isko hame swap karna padega
     let temp = data[minId];
     data[minId] = data[i];
     data[i] = temp;
@@ -19,7 +17,3 @@ function SelectionSort(data) {
 
 SelectionSort(items);
 console.log(items);
-
-// Complexity when we use 2 loops(loops pure chal rahe honge) O(n^^2)
-//One time loop = N
-//Two time loop = (n^2)
