f7fd3d86e0192f1c8539a894f38e4a275566253d
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-06-15 19:21:41 +0530

    Map

--- 18.  Map Data Structure/index.js
+++ 18.  Map Data Structure/index.js
@@ -4,42 +4,20 @@ let data = new Map([
   [100, "number"],
 ]);
 
-//! How to add value in it
-// data.set("age", 19);
-//! How to check size
-// console.log(data.size)  // 4
-
-//! ye key hai ya nhi hai map mai
-// console.log(data.has(100)) // true
-
-//! how to get value in it
-// console.log(data.get(100)) // true
-
-//! how to clear data
-// data.clear() //this will delete our value from map
-
-
-//! how can i apply loop 
-// ab idr 2 loop lag sakte hai for of loop or for each loop
-
-for(x of data) {
-    // console.log(x) // [ 'name', 'test' ] ,[ true, 'bool key' ],[ 100, 'number' ]
-    //! this will return keys
-    // console.log(x[0])  // name, true,100
-    //! this will return values
-    // console.log(x[1]) // test, boolkey,number
-    
-}
-
+data.set("color", "red");
+// console.log(data.size);
 // console.log(data);
+// console.log(data.get(true));
 
-//! For Each Value
-data.forEach((val, key) =>{
-    // console.log(val) // test, boolkey,number
-    // console.log(key) // name,true,100
-})
+// data.clear()
 
+// for(x of data){
+//   console.log(x)
+// }
 
-//! This is how you can get single value
-console.log(data.get("name")) //test
-// console.log(name)
+// data.forEach((val, key) => {
+//   console.log(key);
+// });
+
+data.delete("name");
+console.log(data);
--- 18.  Map Data Structure/notes.md
+++ 18.  Map Data Structure/notes.md
@@ -27,3 +27,54 @@ Map(4) {
 }
 
 
+
+=============
+
+let data = new Map([
+  ["name", "test"],
+  [true, "bool key"],
+  [100, "number"],
+]);
+
+//! How to add value in it
+// data.set("age", 19);
+//! How to check size
+// console.log(data.size)  // 4
+
+//! ye key hai ya nhi hai map mai
+// console.log(data.has(100)) // true
+
+//! how to get value in it
+// console.log(data.get(100)) // true
+
+//! how to clear data
+// data.clear() //this will delete our value from map
+
+
+//! how can i apply loop 
+// ab idr 2 loop lag sakte hai for of loop or for each loop
+
+for(x of data) {
+    // console.log(x) // [ 'name', 'test' ] ,[ true, 'bool key' ],[ 100, 'number' ]
+    //! this will return keys
+    // console.log(x[0])  // name, true,100
+    //! this will return values
+    // console.log(x[1]) // test, boolkey,number
+    
+}
+
+// console.log(data);
+
+//! For Each Value
+data.forEach((val, key) =>{
+    // console.log(val) // test, boolkey,number
+    // console.log(key) // name,true,100
+})
+
+
+//! This is how you can get single value
+console.log(data.get("name")) //test
+// console.log(name)
+
+data.delete("name") :- deleted one specific key
+Map(3) { true => 'bool key', 100 => 'number', 'color' => 'red' }
